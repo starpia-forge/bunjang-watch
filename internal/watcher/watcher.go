ee4e983a955bc2dd7140e2ec5ce2b3830e829085
StarpiaForge <starpia.forge@gmail.com>
2025-04-09 23:49:02 +0900

    Watch 메서드 기능 가능하도록 수정

--- internal/watcher/watcher.go
+++ internal/watcher/watcher.go
@@ -2,11 +2,12 @@ package watcher
 
 import (
 	"context"
-	"github.com/starpia-forge/bunjang-watch/internal/watcher/api"
+	"github.com/starpia-forge/bunjang-watch/internal/watcher/api/v1"
+	"time"
 )
 
 type Watcher interface {
-	Watch(ctx context.Context) ([]api.Product, error)
+	Watch(ctx context.Context) (chan []v1.Product, error)
 }
 
 type watcher struct {
@@ -27,6 +28,34 @@ func NewWatcherWithConfig(c *WatcherConfig) Watcher {
 	return w
 }
 
-func (w *watcher) Watch(context.Context) ([]api.Product, error) {
-	return []api.Product{}, nil
+func (w *watcher) Watch(ctx context.Context) (chan []v1.Product, error) {
+	out := make(chan []v1.Product)
+
+	go func() {
+		ticker := time.NewTicker(w.Interval)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ctx.Done():
+				// TODO - Error Handling
+				return
+			case <-ticker.C:
+			}
+			products, err := w.watch(ctx)
+			if err != nil {
+				// TODO - Error Handling
+			}
+			out <- products
+		}
+	}()
+
+	return out, nil
+}
+
+func (w *watcher) watch(ctx context.Context) ([]v1.Product, error) {
+	products, err := v1.Query(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
 }
