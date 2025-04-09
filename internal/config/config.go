99937d2ced223a4bdb61eb4d4e11f896492bb515
StarpiaForge <starpia.forge@gmail.com>
2025-04-09 22:46:55 +0900

    사용자로 부터 입력 받는 설정을 별도의 config 패키지로 분리하여 관리 하도록 수정 - 이에 따라 기존 watcher 패키지의 Config 구조체는 internal/config 경로로 이동

---
+++ internal/config/config.go
@@ -0,0 +1,15 @@
+package config
+
+import "time"
+
+type Config struct {
+	Interval time.Duration `json:"interval"`
+	Filter
+}
+
+type Filter struct {
+	Keywords     []string
+	MinimumPrice int
+	MaximumPrice int
+	IncludeUsed  bool
+}
