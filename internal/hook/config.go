cb47f0cb001a055e793780a1b61f4b4382fd54b8
starpia-forge <starpia.forge@gmail.com>
2025-04-15 23:38:22 +0900

    HookConfig - Timeout 필드 추가, json 마샬링 태그 추가

--- internal/hook/config.go
+++ internal/hook/config.go
@@ -1,7 +1,10 @@
 package hook
 
+import "time"
+
 type HookConfig struct {
-	Type string
-	Name string
-	URL  string
+	Type    string        `json:"type,omitempty"`
+	Name    string        `json:"name,omitempty"`
+	URL     string        `json:"url,omitempty"`
+	Timeout time.Duration `json:"timeout,omitempty"`
 }
